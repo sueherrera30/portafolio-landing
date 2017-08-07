b60188af6c9cb6965964894c9781748d590e6b41
sue martinez <sueherrera30@gmail.com>
2017-08-07 02:11:23 +0000

    version desktop hasta habilidades tecnicas

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.subtitulo-principal--txt', {
+	strings: ["Front-end Developer jr."],
+	typeSpeed: 80,
+	loop: true,
+});
+
+/*
+var typed = new Typed('.typing-titulos', {
+	strings: ["!Hola, mucho gusto!"],
+	typeSpeed: 80,
+	loop: false,
+});
+*/
+
